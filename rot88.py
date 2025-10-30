9b8550782b840feb25da2a41ddea15dc794872ce
Andrew Taylor <rolyata47@gmail.com>
2025-10-30 06:23:19 -0500

    Migrating rot88.py to argparse and adding a positional argument

--- rot88.py
+++ rot88.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 
 def rot88(message):
     result = ""
@@ -8,14 +8,21 @@ def rot88(message):
         result += chr(code)
     return result
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python rot88.py <msgfile>")
-        sys.exit(0)
-    file = open(sys.argv[1], "r")
-    message = file.read()
-    result = rot88(message)
-    print(result, end="")
-
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(prog="rot88.py", description="Encrypt or decrypt a message using rot88")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("message", type=str, nargs="?")
+    group.add_argument("-m", "--msgfile", type=str)
+    parser.add_argument("-o", "--output", type=str)
+    args = parser.parse_args()
+    if args.msgfile:
+        with open(args.msgfile, "r") as msgfile:
+            message = msgfile.read()
+    else:
+        message = args.message
+    output = rot88(message)
+    if args.output:
+        with open(args.output, "w") as outputfile:
+            outputfile.write(output)
+    else:
+        print(output, end="")
