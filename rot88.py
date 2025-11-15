47fd2eac24bfd9b0f43782f96dc4849c70dd51a5
Andrew Taylor <rolyata47@gmail.com>
2025-11-15 10:39:02 -0600

    Standardizing command line arguments across different files

--- rot88.py
+++ rot88.py
@@ -3,28 +3,28 @@
 import argparse
 
 def rot88(message):
-    result = ""
+    result = ''
     for character in message:
         code = ord(character)
         code = (code + 0x88000) % 0x110000
         result += chr(code)
     return result
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog="rot88.py", description="Encrypt or decrypt a message using rot88")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='rot88.py', description='Encrypt or decrypt a message using rot88')
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("message", type=str, nargs="?")
-    group.add_argument("-m", "--msgfile", type=str)
-    parser.add_argument("-o", "--output", type=str)
+    group.add_argument('message', type=str, nargs='?')
+    group.add_argument('-i', '--inputfile', type=str)
+    parser.add_argument('-o', '--outputfile', type=str)
     args = parser.parse_args()
-    if args.msgfile:
-        with open(args.msgfile, "r") as msgfile:
-            message = msgfile.read()
+    if args.inputfile:
+        with open(args.inputfile, 'r') as file:
+            message = file.read()
     else:
         message = args.message
     output = rot88(message)
-    if args.output:
-        with open(args.output, "w") as outputfile:
-            outputfile.write(output)
+    if args.outputfile:
+        with open(args.outputfile, 'w') as file:
+            file.write(output)
     else:
-        print(output, end="")
+        print(output, end='')
