3e500e0441a2f9454cad4ec4f7b3cf234ae17333
Andrew Taylor <rolyata47@gmail.com>
2026-01-29 05:04:15 -0600

    Simplifying unit tests

--- tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-
-project_root = Path(__file__).parent.parent
-test_data_path = project_root / 'tests' / 'test_data'
--- tests/test_rot88.py
+++ tests/test_rot88.py
@@ -1,23 +1,22 @@
 from rot88 import rot88
-from tests import test_data_path
 from unittest import TestCase
 
 class TestRot88(TestCase):
 
-    def process_file(self, filename):
-        with open(test_data_path / filename, 'r') as file:
+    def process_file(self, path):
+        with open(path, 'r') as file:
             message = file.read()
         ciphertext = rot88(message)
         assert rot88(ciphertext) == message
 
     def test_message(self):
-        self.process_file('message.txt')
+        self.process_file('tests/test_data/message.txt')
 
     def test_equations(self):
-        self.process_file('equations.txt')
+        self.process_file('tests/test_data/equations.txt')
 
     def test_pirsquared(self):
-        self.process_file('pirsquared.txt')
+        self.process_file('tests/test_data/pirsquared.txt')
 
     def test_spanish(self):
-        self.process_file('spanish.txt')
+        self.process_file('tests/test_data/spanish.txt')
